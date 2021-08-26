625eccee8d83eaba309eef6e90398d62101ccefe
wagnerxavierp <wagner3337@gmail.com>
2021-08-26 01:41:01 -0300

    gitflow-hotfix-stash: 2.0.3
    criando uma hotfix

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.red),
       home: HomePage(),
     );
